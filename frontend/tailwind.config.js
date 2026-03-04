4b2fe887b965e66de0903f8bd918bfcaadb5fd04
Jung-soyoung <mansionavigati@gmail.com>
2026-03-04 11:54:02 +0900

    [fix] 전역 폰트 nanum gothic으로 수정

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 const withMT = require("@material-tailwind/react/utils/withMT");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Nanum Gothic"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 });
